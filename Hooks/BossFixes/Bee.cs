8941799ae18a5a1032edc780d10e6ccbe17e0a57
vtvrv <98350357+vtvrv@users.noreply.github.com>
2022-06-30 11:30:46 +0000

    Upload BossFixes

---
+++ Hooks/BossFixes/Bee.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CupheadQOL.Hooks.BossFixes
+{
+    internal class Bee
+    {
+        //Destroy the animated honey on the borders
+        [HarmonyPostfix, HarmonyPatch(typeof(BeeLevelBackground), "LevelInit")]
+        private static void LevelInit(BeeLevelBackground __instance)
+        {
+            GameObject honeyObj = GameObject.Find("Honey");
+            if (honeyObj != null)
+                GameObject.Destroy(honeyObj);
+        }
+
+        //Destroy honey dripping from top of level
+        [HarmonyPrefix, HarmonyPatch(typeof(BeeLevelHoneyDrip), "Awake")]
+        private static void Awake(BeeLevelHoneyDrip __instance)
+        {
+            GameObject.Destroy(__instance.gameObject);
+        }
+
+        //Stop animations of platforms
+        [HarmonyPostfix, HarmonyPatch(typeof(BeeLevelPlatforms), "Init")]
+        private static void Init(BeeLevelPlatforms __instance)
+        {
+            Transform[] rows = Traverse.Create(__instance).Field("rows").GetValue() as Transform[];
+            foreach(Transform row in rows)
+            {
+                foreach (var component in row.gameObject.GetComponentsInChildren<AnimationHelper>())
+                    GameObject.Destroy(component);
+                foreach (var component in row.gameObject.GetComponentsInChildren<UnityEngine.Animator>())
+                    GameObject.Destroy(component);
+            }
+        }
+        
+    }
+}
---
+++ Hooks/BossFixes/Dragon.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CupheadQOL.Hooks.BossFixes
+{
+    internal class Dragon
+    {
+        //BUG. One frame of lightning plays at the beginning of the final phase. 
+        [HarmonyPrefix, HarmonyPatch(typeof(DragonLevelLightning), "PlayLightning")]
+        private static void PlayLightning(DragonLevelLightning __instance)
+        {
+            GameObject.Destroy(__instance.gameObject);
+            return;
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(DragonLevelRain), "StartRain")]
+        private static void StartRain(DragonLevelRain __instance)
+        {
+            GameObject.Destroy(__instance.gameObject);
+            return;
+        }
+    }
+}
