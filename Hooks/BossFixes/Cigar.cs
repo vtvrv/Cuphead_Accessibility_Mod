cfcebb8468691ea4ee57937aadd927620add787a
vtvrv <98350357+vtvrv@users.noreply.github.com>
2022-07-06 18:03:10 +0000

    Add files via upload

--- Hooks/BossFixes/Cigar.cs
+++ Hooks/BossFixes/Cigar.cs
@@ -1,4 +1,4 @@
-using HarmonyLib;
+﻿using HarmonyLib;
 using UnityEngine;
 
 //"Level/Background/FireFG"
---
+++ Hooks/BossFixes/FlyingHorse.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CupheadQOL.Hooks.BossFixes
+{
+    internal class FlyingHorse
+    {
+        private static void DisableObj(string objName)
+        {
+            var obj = GameObject.Find(objName);
+            if (obj != null)
+            {
+                obj.SetActive(false);
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(DicePalaceFlyingHorseLevel), "Start")]
+        private static void Start(DicePalaceFlyingHorseLevel __instance)
+        {
+            DisableObj("Level/Foreground/kd_horse_bg_near_big_porch");
+            DisableObj("Level/Foreground/kd_horse_bg_near_bleacher");
+            DisableObj("Level/Foreground/kd_horse_bg_near_lamp_01");
+            DisableObj("Level/Foreground/kd_horse_bg_near_lamp_02 (1)");
+            DisableObj("Level/Foreground/kd_horse_bg_near_lamp_02");
+            DisableObj("Level/Foreground/kd_horse_bg_near_trees_01-loop");
+            DisableObj("Level/Foreground/kd_horse_bg_near_trees_02-loop");
+            DisableObj("Level/Foreground/kd_horse_bg_near_trees_03-loop");
+            DisableObj("Level/Foreground/kd_horse_bg_near_very_close_trees_01");
+            DisableObj("Level/Foreground/kd_horse_bg_near_very_close_trees_02");
+        }
+    }
+}
