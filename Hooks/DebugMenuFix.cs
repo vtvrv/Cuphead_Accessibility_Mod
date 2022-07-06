d564e03c96f6ce0863a8b448ceb9e92e2b695521
vtvrv <98350357+vtvrv@users.noreply.github.com>
2022-07-06 18:02:43 +0000

    Add files via upload

---
+++ Hooks/DebugMenuFix.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CupheadQOL.Hooks
+{
+    internal class DebugMenuFix
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(LevelSelectScene), "Update")]
+        private static void Update(LevelSelectScene __instance)
+        {
+            Cursor.visible = true;
+            //Still freezes after a few seconds
+            //Something to do with event system
+            //Doesn't freeze when UnityExplorer is open https://github.com/sinai-dev/UnityExplorer
+            //See https://github.com/sinai-dev/UniverseLib/blob/main/src/Input/CursorUnlocker.cs
+        }
+    }
+}
---
+++ Hooks/MuteAnnouncer.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace CupheadQOL.Hooks
+{
+    internal class MuteAnnouncer
+    {
+        private static void DisableObj(string objName)
+        {
+            var obj = GameObject.Find(objName);
+            if (obj != null)
+            {
+                obj.SetActive(false);
+            }
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(Level), "PlayAnnouncerReady")]
+        private static void PlayAnnouncerReady(Level __instance)
+        {
+            DisableObj("Level_Audio(Clone)/Announcer/Begin");
+            DisableObj("Level_Audio(Clone)/Announcer/Ready");
+            return;
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(Level), "PlayAnnouncerBegin")]
+        private static void PlayAnnouncerBegin(Level __instance)
+        {
+            DisableObj("Level_Audio(Clone)/Announcer/Begin");
+            DisableObj("Level_Audio(Clone)/Announcer/Ready");
+            return;
+        }
+    }
+}
